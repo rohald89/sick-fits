fb61a90684a3cd26746c1a74c12bed363a6542c7
Rohald van Merode <rohald_89@live.nl>
2021-04-21 19:58:54 +0200

    added NavStyles

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "07bd62eec4de224c9af3";
+/******/ 	var hotCurrentHash = "b797ad99be19553611fd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -23,7 +23,7 @@ const HeaderStyles = styled.header`
     display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
-    align-items: center;
+    align-items: stretch;
   }
 
   .sub-bar {
@@ -39,11 +39,11 @@ const Header = () => (
       <Logo>
         <Link href="/">Sick Fits</Link>
       </Logo>
+      <Nav />
     </div>
     <div className="sub-bar">
       <p>search</p>
     </div>
-    <Nav />
   </HeaderStyles>
 );
 
