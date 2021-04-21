8374cb468fe717c7229c91984c9b4744279ef5bf
Rohald van Merode <rohald_89@live.nl>
2021-04-21 22:00:18 +0200

    created mutation to submit form and create a new product on the backend

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "81017a77f3ff76289bd3";
+/******/ 	var hotCurrentHash = "e3947efcab851609a015";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
