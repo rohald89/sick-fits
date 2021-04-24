a33532b7a08e507e466c00a7435e1370d8ba1a22
Rohald van Merode <rohald_89@live.nl>
2021-04-24 17:23:25 +0200

    added dynamic routing for the pagination

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "238cd794b0d741b20e0a";
+/******/ 	var hotCurrentHash = "d9e499725726aad52036";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
