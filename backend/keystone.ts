f1ed5d48b4312adbbbd510bf8356ed2ce84c98ee
Rohald van Merode <rohald_89@live.nl>
2021-05-02 21:19:48 +0200

    created CartItem schema and set up relationships for product and user

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -7,6 +7,7 @@ import {
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
+import { CartItem } from './schemas/CartItem';
 import { ProductImage } from './schemas/ProductImage';
 import { insertSeedData } from './seed-data';
 import { sendPassWordResetEmail } from './lib/mail';
@@ -58,6 +59,7 @@ export default withAuth(
       User,
       Product,
       ProductImage,
+      CartItem,
     }),
     ui: {
       // Show the UI only for people that pass this test
--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -4,11 +4,19 @@ import { text, password, relationship } from '@keystone-next/fields';
 // named export
 export const User = list({
   // access:
-  // ui
+
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
     password: password(),
+    cart: relationship({
+      ref: 'CartItem.user',
+      many: true,
+      ui: {
+        createView: { fieldMode: 'hidden' },
+        itemView: { fieldMode: 'read' },
+      },
+    }),
     // TODO, add roles, cart and orders
   },
 });
