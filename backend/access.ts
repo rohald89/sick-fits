3b224d80b0bff7a229a12377e6c34dbd641937c6
Rohald van Merode <rohald_89@live.nl>
2021-05-16 12:51:08 +0200

    created permission access functions

--- backend/access.ts
+++ backend/access.ts
@@ -1,7 +1,27 @@
 // At it's simplest, the access control return sa yes or no value depending on the users session
 
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// Permissions check if someone meets a criteria - yes or no.
+export const permissions = {
+  ...generatedPermissions,
+  isAwesome({ session }: ListAccessArgs): boolean {
+    return session?.data.name.includes('Rohald');
+  },
+};
+
+// Rule based function
--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5ea0e6a2ec2447e55b7c";
+/******/ 	var hotCurrentHash = "c31538d2e68ecd49fab8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
