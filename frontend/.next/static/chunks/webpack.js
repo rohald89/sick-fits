8f4b2afc888f982f3fdf2aff2cf5b3dc2b7d4db0
Rohald van Merode <rohald_89@live.nl>
2021-05-16 14:36:14 +0200

    made few adjustments so users not currently signed in can still see products and product pages

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3337ddc7d031060f7ed3";
+/******/ 	var hotCurrentHash = "b1904a13e9f12ce720f4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
