53c26789f0c1881eab7973f1d955ae7c1ad21a5c
Rohald van Merode <rohald_89@live.nl>
2021-10-27 19:17:51 +0200

    created snapshottest for Product component

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cb5b98b90a8051589224";
+/******/ 	var hotCurrentHash = "c18c15cac35732586c9f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
